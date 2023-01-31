4eeab6dd4ba6ab9c28d2da8b2a11e7159ec946e6
mwszekely <19625841+mwszekely@users.noreply.github.com>
2023-01-31 18:59:55 -0500

    Use pnpm instead of npm as the package manager

--- docs/rollup.config.mjs
+++ docs/rollup.config.mjs
@@ -3,6 +3,9 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from '@rollup/plugin-typescript';
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import path from "path";
+import sourcemaps from "rollup-plugin-sourcemaps"
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
 export default {
     input: "main.tsx",
@@ -13,13 +16,14 @@ export default {
         sourcemap: "inline"
     },
     plugins: [
-        typescript(),
-        commonjs(),
-        resolve({ dedupe: ['preact', "preact/compat", "preact/hooks"] }),
+        typescript({ sourceMap: true }), 
+        commonjs({ sourceMap: true, extensions }), 
+        resolve({ extensions }), 
         getBabelOutputPlugin({
             configFile: path.resolve(".babelrc"),
             sourceMaps: true,
             allowAllFormats: true
         }),
+        sourcemaps()    // TODO: This is deprecated but needed for TS source maps
     ],
 }
